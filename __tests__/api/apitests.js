c3c8291d5a3de6ccd160324de3f36c6abf8fde12
nigilan <nigilan>
2019-07-14 21:08:17 +0530

    Lowercase user name fix in server side

--- __tests__/api/apitests.js
+++ __tests__/api/apitests.js
@@ -31,3 +31,13 @@ it('data found when given right values', function () {
         expect(bodyValues.data).toStrictEqual([1000223232,762183618236,962384672364]);
       });
 });
+
+it('case sensitive issue for users', function () {
+  return frisby.get(serverUrl+'accounts/Nigilan')
+  .then(function (res) {
+    const bodyValues = JSON.parse(res._body);
+    expect(res.status).toBe(200);
+    expect(bodyValues.errMsg).toBe('');
+    expect(bodyValues.data).toStrictEqual([1000223232,762183618236,962384672364]);
+  });
+});
--- routes/accounts.js
+++ routes/accounts.js
@@ -3,11 +3,12 @@ var router = express.Router();
 const modelAccounts = require('../models/accountdetails');
 
 router.get('/:user', function(req, res, next) {
-  const {user} = req.params;
+  let {user} = req.params;
   if (!user){
     res.status(500).send({data:'', errMsg : 'Internal Server Error!!'});
     return;
   }
+  user = user.toLowerCase();
   const accounts = modelAccounts.getAccountDetails(user);
   if (!accounts || accounts.length === 0){
     res.status(401).send({data:[], errMsg : 'No data found!'});
